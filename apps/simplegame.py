b3b6534e32ab34e363f6954c998eee66cd1f886b
James <james@mathews.top>
2019-04-17 21:44:06 +0300

    make horse list

--- apps/simplegame.py
+++ apps/simplegame.py
@@ -1,5 +1,5 @@
 from ti700.app import TerminalApp
-from random import randint
+import random
 
 class SimpleGame(TerminalApp):
     money = 100
@@ -7,10 +7,13 @@ class SimpleGame(TerminalApp):
     name = ""
 
     horses = [
-        'Magic horse the fastest horse',
-        'Crazy horse the craziest horse',
-        'Stupid horse the stupidest horse',
-        'Silly horse the one you lease expect',
+        'Magic horse',
+        'Crazy horse',
+        'Stupid horse',
+        'Silly horse',
+        'Wonder horse',
+        'Foxy horse',
+        'Triuphant horse'
     ]
 
     def start(self):
@@ -27,14 +30,15 @@ class SimpleGame(TerminalApp):
             key = None
             self.send("New race!!!")
             self.send("a) place a bet, b) watch race")
+            horse_list = self.generate_list()
             while key != 'a' and key != 'b':
                 key = self.read_key(" ? ").lower()
             self.send("")
             if key == 'b':
-                winner = self.race()
+                winner = self.race(horse_list)
             if key == 'a':
-                horse, bet = self.get_bet()
-                winner = self.race()
+                horse, bet = self.get_bet(horse_list)
+                winner = self.race(horse_list)
                 self.money -= bet
                 if winner != horse:
                     self.send("Unlucky!!!")
@@ -43,75 +47,87 @@ class SimpleGame(TerminalApp):
                 else:
                     self.send("Congratulations!!!")
                     self.sleep(1)
-                    winnings = bet * len(self.horses)
+                    winnings = bet * len(horse_list)
                     self.send("You won ${}".format(winnings))
-                    self.money += bet * len(self.horses)
+                    self.money += bet * len(winnings)
 
         self.send("Sorry you have no more money :(")
         self.sleep(1)
         self.send("Thank you for playing the simple game")
 
+    def generate_list(self, num_horses=4):
+        '''Generates a list of which horses will play'''
+        return random.sample(self.horses, num_horses)
 
     def display_money(self):
         self.send("{}, You have ${}".format(self.name, self.money))
 
-    def get_bet(self):
+    def get_bet(self, horse_list):
         '''Prompts for the users bet and horse'''
         self.send("Which horse do you want to bet on? ")
-        for i, horse in enumerate(self.horses):
+        for i, horse in enumerate(horse_list):
             self.send("{}) {}".format(i+1, horse))
         chosen = None
-        while chosen not in range(1, len(self.horses) + 1):
+        while chosen not in range(1, len(horse_list) + 1):
             key = self.read_key("? ")
             try:
                 chosen = int(key)
             except ValueError:
                 chosen = None
-        chosen = chosen - 1
+        chosen = horse_list[chosen - 1]
 
-        self.send("\nAnd how much do you want to bet on {}?".format(self.horses[chosen]))
+        self.send("\nAnd how much do you want to bet on {}?".format(chosen))
         bet = None
         while bet is None:
             read = self.prompt("$")
             try:
                 bet = float(read)
             except ValueError:
-                bet = Nobne
+                bet = None
             if bet and  bet > self.money:
                 self.send("{} you only have ${}".format(self.name, self.money))
                 bet = None
         return chosen, bet
 
-    def random_horse(self):
-        return randint(0, len(self.horses)-1)
 
-    def random_name(self):
-        return self.horses[self.random_horse()]
 
-    def race(self):
+    def race(self, horse_list):
         short = lambda x: x.split(" ")[0]
-        winner = self.random_horse()
-        winner_name = self.horses[winner]
-        winner_short = short(winner_name)
+        short_horse = lambda x: short(horse_list[x])
+        random.shuffle(horse_list)
+        winner = horse_list[0]
 
-        random_short = lambda: short(self.random_name())
+        assert len(horse_list) >= 4
+
+        order = [0, 1, 2, 3]
+        random.shuffle(order)
 
         self.sleep(2)
         self.send("And they're off")
         self.sleep(1)
-        self.send("{} is off to a flying start".format(random_short()))
-        self.sleep(3)
-        self.send("{} is in a close second".format(random_short()))
+        self.send("{} is off to a flying start".format(short_horse(order[0])))
         self.sleep(1)
-        self.send("{} is still behind".format(random_short()))
+        self.send("{} is in a close second".format(short_horse(order[1])))
+        self.sleep(1)
+        self.send("{} is still behind".format(short_horse(order[2])))
+        self.sleep(1)
+        self.send("{} is catching up".format(short_horse(order[3])))
         self.sleep(2)
         self.send("But wait.....")
         self.sleep(2)
         self.send("I don't believe this")
+
+        if order[0] == winner:
+            self.sleep(2)
+            self.send("{} has just overtaken".format(short(order[2])))
+            self.sleep(1)
+            self.send("but is falling behind")
+        else:
+            self.sleep(2)
+            self.send("{} has just overtaken".format(short(winner)))
+
         self.sleep(2)
-        self.send("{} has just overtaken".format(winner_short))
-        self.sleep(2)
-        self.send("And the winner is {}".format(winner_name))
+        self.send("And the winner is {}".format(winner))
         return winner
 
 
